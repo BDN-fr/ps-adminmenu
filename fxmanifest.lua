47e2b670c69d33fc0b9834bcda6d3f50cb0e36e3
BDN_fr <lucasjoly2008@gmail.com>
2024-06-20 20:13:45 +0200

    I forgot

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 game "gta5"
 
 author "Project Sloth & OK1ez"
-version '1.0.3'
+version '1.1.6'
 description 'Admin Menu'
 repository 'https://github.com/Project-Sloth/ps-adminmenu'
 
@@ -28,6 +28,8 @@ shared_script {
 
 files {
   'html/**',
+  'data/ped.lua',
+  'data/object.lua',
   'locales/*.json',
 }
 
